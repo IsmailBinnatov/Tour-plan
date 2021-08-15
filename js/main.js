d521481b54cb71846a458c45fa62a6790baf39d6
Ismll <ivindend@mail.ru>
2021-08-15 11:49:35 +0400

    добавил swiper слайдер в секцию Отель

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const swiper = new Swiper('.swiper-container', {
+
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    speed: 2000,
+    
+    effect: 'coverflow',
+    coverflowEffect: {
+        rotate: 30,
+        slideShadows: false,
+      },
+      
+    autoplay: {
+        delay: 5000,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.slider-button--next',
+      prevEl: '.slider-button--prev',
+    },
+
+  });
