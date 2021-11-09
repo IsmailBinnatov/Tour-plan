3ad28fb3a2a516db06c1b494039a918a43988e5c
Ismll <ivindend@mail.ru>
2021-11-09 22:54:48 +0400

    создал мобильное меню

--- js/main.js
+++ js/main.js
@@ -52,3 +52,11 @@ const hotelSwiper = new Swiper('.hotel-slider', {
     },
 
   });
+
+
+
+var menuButton = document.querySelector('.menu-button')
+
+menuButton.addEventListener('click', function() {
+  document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom--visible')
+})
